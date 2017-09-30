791f8d56410120655ceb8b47f13606a3ec8dca5c
Nathaniel Saul <nat@saulgill.com>
2017-09-30 01:27:03 -0700

    Update examples to new interface

--- examples/cat/cat.py
+++ examples/cat/cat.py
@@ -2,13 +2,19 @@ import km
 
 data = km.np.genfromtxt('cat-reference.csv',delimiter=',')
 
-mapper = km.KeplerMapper(cluster_algorithm=km.cluster.DBSCAN(eps=0.1, min_samples=5), nr_cubes=10, overlap_perc=0.8, verbose=1)
+mapper = km.KeplerMapper()
 
-mapper.fit(data)
 
-complex = mapper.map(data, dimension_index=1, dimension_name="Y-axis")
+lens = mapper.fit_transform(data)
 
-mapper.visualize(complex, "cat_keplermapper_output.html", "cat-reference.csv")
+graph = mapper.map(lens,
+                   data,
+                   clusterer=km.cluster.DBSCAN(eps=0.1, min_samples=5),
+                   nr_cubes=15,
+                   overlap_perc=0.2)
+
+mapper.visualize(graph,
+                path_html="cat_keplermapper_output.html")
 
 # You may want to visualize the original point cloud data in 3D scatter too
 """

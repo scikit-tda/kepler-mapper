ae80d97af91eea84f354bbd7b43cf91fd42e6f2d
Nathaniel Saul <nat@saulgill.com>
2017-11-13 14:50:35 -0800

    print details in benchmark

--- test/benchmarks.py
+++ test/benchmarks.py
@@ -10,23 +10,25 @@ Should provide summary of
 
 """
 
-
 import cProfile, pstats, io
-
-
 
 import time
 
 import numpy as np
 from sklearn import datasets
-
 from kmapper import KeplerMapper
 
 
 def profile():
+    num_sets = 100
+    blob_size = 1000
+    nr_cubes = 10
+    overlap = 0.2
+
+
     blob_list = []
-    for i in range(100):
-        data, _ = datasets.make_blobs(100)
+    for i in range(num_sets):
+        data, _ = datasets.make_blobs(blob_size)
         blob_list.append(data)
 
     mapper = KeplerMapper(verbose=0)
@@ -38,17 +40,16 @@ def profile():
         lens = mapper.fit_transform(data)
         graph = mapper.map(lens,
                            data,
-                           nr_cubes=10,
-                           overlap_perc=0.2)
+                           nr_cubes=nr_cubes,
+                           overlap_perc=overlap)
 
     pr.disable()
     s = io.StringIO()
     sortby = 'cumulative'
     ps = pstats.Stats(pr, stream=s).strip_dirs().sort_stats(sortby)
     ps.print_stats("kmapper")
+    print("Ran {} blobs of size {} with params (nr_cubes:{}\toverlap:{})".format(num_sets, blob_size, nr_cubes, overlap))
     print(s.getvalue())
 
-
 if __name__ == "__main__":
-
     profile()

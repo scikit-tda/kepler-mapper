c4b4546347a0951a802a5944f9deb9e27feb02cf
Nathaniel Saul <nat@saulgill.com>
2017-11-13 15:39:32 -0800

    fixup! updated setup instructions and package requirements

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from distutils.core import setup
 setup(name='kmapper',
       version='0.2.1',
       description='Kepler-mapper',
-      author='Nathaniel Saul',
-      author_email='nat@saulgill.com',
+      author='HJ van Veen',
+      author_email='info@mlwave.com',
       url='https://github.com/MLWAve/kepler-mapper',
       packages=['kmapper'],
       install_requires=[

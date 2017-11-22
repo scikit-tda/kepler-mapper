e3bb8a1af8baa4d094f17697c432124553367126
Nathaniel Saul <nat@saulgill.com>
2017-11-21 19:00:37 -0800

    rework cover api. can pass in cover object

---
+++ test/test_coverer.py
@@ -0,0 +1,122 @@
+import pytest
+import numpy as np
+
+import kmapper as km
+
+from kmapper.kmapper import Cover
+
+from sklearn import datasets
+
+
+class TestCover():
+
+    def test_multibins(self):
+        data = np.arange(30).reshape(10, 3)
+        c = Cover(nr_cubes=[5, 10])
+        cubes = c.define_bins(data)
+        assert len(list(cubes)) == 5 * 10
+
+    def test_cube_count(self):
+        data = np.arange(30).reshape(10, 3)
+        c = Cover(nr_cubes=10)
+        cubes = c.define_bins(data)
+
+        assert len(list(cubes)) == 10**2, "idx column is ignored"
+
+    def test_cube_dim(self):
+
+        data = np.arange(30).reshape(10, 3)
+        c = Cover(nr_cubes=10)
+        cubes = c.define_bins(data)
+
+        assert all(len(cube) == 2 for cube in cubes)
+
+    def test_single_dim(self):
+        data = np.arange(20).reshape(10, 2)
+        c = Cover(nr_cubes=10)
+        cubes = c.define_bins(data)
+
+        assert all(len(cube) == 1 for cube in cubes)
+
+    def test_chunk_dist(self):
+        data = np.arange(20).reshape(10, 2)
+
+        cover = Cover(nr_cubes=10)
+        _ = cover.define_bins(data)
+        chunks = list(cover.chunk_dist)
+        # TODO: this test is really fagile and has magic number, fix.
+        assert all(i == 1.8 for i in chunks)
+
+    def test_nr_dimensions(self):
+        data = np.arange(30).reshape(10, 3)
+
+        c = Cover(nr_cubes=10)
+        _ = c.define_bins(data)
+        assert c.nr_dimensions == 2
+
+    def test_bound_is_min(self):
+        data = np.arange(30).reshape(10, 3)
+        cov = Cover(nr_cubes=10)
+        _ = cov.define_bins(data)
+        bounds = list(zip(cov.d, range(1, 10)))
+        assert all(b[0] == b[1] for b in bounds)
+
+    def test_entries_even(self):
+        data = np.arange(40).reshape(20, 2)
+
+        cover = Cover(nr_cubes=10)
+        cubes = cover.define_bins(data)
+
+        for cube in cubes:
+            entries = cover.find_entries(data, cube)
+
+            assert len(entries) >= 2
+
+    def test_entries_in_correct_cubes(self):
+        # TODO: this test is a little hacky
+
+        data = np.arange(40).reshape(20, 2)
+
+        cover = Cover(nr_cubes=10)
+        cubes = cover.define_bins(data)
+        cubes = list(cubes)
+        # import pdb; pdb.set_trace()
+        entries = [cover.find_entries(data, cube) for cube in cubes]
+
+        # inside of each cube is there. Sometimes the edges don't line up.
+        for i in range(10):
+            assert data[2 * i] in entries[i]
+            assert data[2 * i + 1] in entries[i]
+
+    def test_cubes_overlap(self):
+        data = np.arange(40).reshape(20, 2)
+
+        cover = Cover(nr_cubes=10)
+        cubes = cover.define_bins(data)
+
+        entries = []
+        for cube in cubes:
+            # turn singleton lists into individual elements
+            res = [i[0] for i in cover.find_entries(data, cube)]
+            entries.append(res)
+
+        for i, j in zip(range(9), range(1, 10)):
+            assert set(entries[i]).union(set(entries[j]))
+
+
+
+def test_BasicCover():
+    # TODO: add a mock that asserts the cover was called appropriately.. or test number of cubes etc.
+    data, _ = datasets.make_circles()
+    mapper = km.KeplerMapper()
+    graph = mapper.map(data)
+    mapper.visualize(graph)
+
+# def test_diffCover():
+#     data, _ = datasets.make_circles()
+#
+#
+#     mapper = km.KeplerMapper()
+#     cover = km.DiffDimCover(data, nr_cubes=[3,4], overlap_perc=[0.2, 0.4])
+#     graph = mapper.map(data, coverer=cover)
+#     mapper.visualize(graph)
--- test/test_mapper.py
+++ test/test_mapper.py
@@ -3,7 +3,25 @@ import numpy as np
 
 from kmapper import KeplerMapper
 
-from kmapper.kmapper import Cover
+
+class TestLogging():
+    """ Simple tests that confirm map completes at each logging level
+    """
+
+    def test_runs_with_logging_0(self):
+        mapper = KeplerMapper(verbose=0)
+        data = np.random.rand(100, 2)
+        graph = mapper.map(data)
+
+    def test_runs_with_logging_1(self):
+        mapper = KeplerMapper(verbose=1)
+        data = np.random.rand(100, 2)
+        graph = mapper.map(data)
+
+    def test_runs_with_logging_2(self):
+        mapper = KeplerMapper(verbose=2)
+        data = np.random.rand(100, 2)
+        graph = mapper.map(data)
 
 
 class TestVisualize():
@@ -20,7 +38,7 @@ class TestVisualize():
         viz1 = mapper.visualize(graph, path_html=file.strpath)
 
         new_mapper = KeplerMapper()
-        viz2 = new_mapper.visualize(graph, path_html= file.strpath)
+        viz2 = new_mapper.visualize(graph, path_html=file.strpath)
 
         assert viz1 == viz2
 
@@ -50,11 +68,12 @@ class TestVisualize():
         assert len(tmpdir.listdir()) == 0, "file was never written to"
         # assert file.read() != viz
 
+
 class TestLinker():
     def test_finds_a_link(self):
         mapper = KeplerMapper()
 
-        groups = {"a": [1,2,3,4], "b":[1,2,3,4]}
+        groups = {"a": [1, 2, 3, 4], "b": [1, 2, 3, 4]}
         links = mapper._create_links(groups)
 
         assert "a" in links or "b" in links
@@ -63,7 +82,7 @@ class TestLinker():
     def test_no_link(self):
         mapper = KeplerMapper()
 
-        groups = {"a": [1,2,3,4], "b":[5,6,7]}
+        groups = {"a": [1, 2, 3, 4], "b": [5, 6, 7]}
         links = mapper._create_links(groups)
 
         assert not links
@@ -71,98 +90,12 @@ class TestLinker():
     def test_pass_through_result(self):
         mapper = KeplerMapper()
 
-        groups = {"a": [1], "b":[2]}
+        groups = {"a": [1], "b": [2]}
 
         res = dict()
         links = mapper._create_links(groups, res)
 
         assert res == links
-
-
-class TestCover():
-    def test_cube_count(self):
-        data = np.arange(30).reshape(10,3)
-        c = Cover(data, nr_cubes=10)
-        cubes = c.cubes
-
-        assert len(cubes) == 10**3
-
-    def test_cube_dim(self):
-
-        data = np.arange(30).reshape(10,3)
-        c = Cover(data, nr_cubes=10)
-        cubes = c.cubes
-
-        assert all( len(cube) == 3 for cube in cubes)
-
-    def test_single_dim(self):
-        data = np.arange(10).reshape(10,1)
-        c = Cover(data, nr_cubes=10)
-        cubes = c.cubes
-
-        assert all( len(cube) == 1 for cube in cubes)
-
-    def test_chunk_dist(self):
-        data = np.arange(100).reshape(10,10)
-
-        cover = Cover(data, nr_cubes=10)
-        chunks = list(cover.chunk_dist)
-
-        assert all(i == 9 for i in chunks)
-
-    def test_nr_dimensions(self):
-        data = np.arange(30).reshape(10,3)
-
-        c = Cover(data, nr_cubes=10)
-
-        assert c.nr_dimensions == 3
-
-    def test_bound_is_min(self):
-        data = np.arange(100).reshape(10,10)
-
-        c = Cover(data, nr_cubes=10)
-
-        bounds = zip(c.d, range(10))
-        assert all(b[0] == b[1] for b in bounds)
-
-    def test_entries_even(self):
-        data = np.arange(20).reshape(20,1)
-
-        cover = Cover(data, nr_cubes=10)
-        cubes = cover._cube_coordinates_all()
-
-        for cube in cubes:
-            entries = cover.find_entries(data, cube)
-
-            assert len(entries) >= 2
-
-    def test_entries_in_correct_cubes(self):
-        data = np.arange(20).reshape(20,1)
-
-        cover = Cover(data, nr_cubes=10)
-        cubes = cover._cube_coordinates_all()
-
-        entries = [cover.find_entries(data, cube) for cube in cubes]
-
-        # inside of each cube is there. Sometimes the edges don't line up.
-        for i in range(10):
-            assert data[2*i] in entries[i]
-            assert data[2*i+1] in entries[i]
-
-    def test_cubes_overlap(self):
-        data = np.arange(20).reshape(20,1)
-
-        cover = Cover(data, nr_cubes=10)
-        cubes = cover._cube_coordinates_all()
-
-        entries = []
-        for cube in cubes:
-            # turn singleton lists into individual elements
-            res = [i[0] for i in cover.find_entries(data, cube)]
-            entries.append(res)
-
-        for i,j in zip(range(9), range(1,10)):
-            assert set(entries[i]).union(set(entries[j]))
 
 
 class TestLens():
@@ -185,12 +118,13 @@ class TestLens():
         assert graph["meta_data"]["scaler"] == "None"
 
     def test_projection(self):
-        atol = 0.1 # accomodate scaling, values are in (0,1), but will be scaled slightly
+        # accomodate scaling, values are in (0,1), but will be scaled slightly
+        atol = 0.1
 
         mapper = KeplerMapper()
         data = np.random.rand(100, 5)
-        lens = mapper.fit_transform(data, projection=[0,1])
-        np.testing.assert_allclose(lens, data[:,:2], atol=atol)
+        lens = mapper.fit_transform(data, projection=[0, 1])
+        np.testing.assert_allclose(lens, data[:, :2], atol=atol)
 
         lens = mapper.fit_transform(data, projection=[0])
-        np.testing.assert_allclose(lens, data[:,:1], atol=atol)
+        np.testing.assert_allclose(lens, data[:, :1], atol=atol)

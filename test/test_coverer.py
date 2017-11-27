b16cd2f9238f31e1011c2de5968cf849c682a6a2
Nathaniel Saul <nat@saulgill.com>
2017-11-27 11:23:17 -0800

    fixup! fix multiple dims in visualize and incorrect defaults

--- test/test_coverer.py
+++ test/test_coverer.py
@@ -38,6 +38,8 @@ class TestAPIMaintenance():
         _ = mapper.map(lens, data, overlap_perc=0.1)
         c2 = mapper.coverer
 
+        assert c2.nr_cubes == 10
+
     def test_no_warn_normally(self, recwarn):
         """ Confirm that deprecation warnings behave as expected"""
         mapper = km.KeplerMapper()

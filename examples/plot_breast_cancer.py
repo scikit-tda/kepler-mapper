7e0e65bfe4a01073011b2c6b3a6ed82e230b6f11
Dave Eargle <dave@daveeargle.com>
2021-01-27 14:45:37 -0700

    add datapoint and node color function examples to breastcancer gallery

--- examples/plot_breast_cancer.py
+++ examples/plot_breast_cancer.py
@@ -10,17 +10,21 @@ This example generates a Mapper built from the `Wisconsin Breast Cancer Dataset 
 
 `Visualization of the breast cancer mapper <../../_static/breast-cancer.html>`_
 
-
 The reasoning behind the choice of lenses in the demonstration above is:
 
-- **For lens1:** Lenses that make biological sense; in other words, lenses that highlight special features in the data, that I know about. 
+- **For lens1:** Lenses that make biological sense; in other words, lenses that highlight special features in the data, that I know about.
 - **For lens2:** Lenses that disperse the data, as opposed to clustering many points together.
 
-In the case of this particualr data, using an anomaly score (in this case calculated using the IsolationForest from sklearn) makes biological sense since cancer cells are anomalous. For the second lens, we use the :math:`l^2` norm.
+In the case of this particular data, using an anomaly score (in this case calculated using the IsolationForest from sklearn) makes biological sense since cancer cells are anomalous. For the second lens, we use the :math:`l^2` norm.
 
 For an interactive exploration of lens for the breast cancer, see the `Choosing a lens notebook <../../notebooks/Cancer-demo.html>`_.
 
+KeplerMapper also permits setting multiple datapoint color functions and node color functions in its html visualizations.
+The example code below demonstrates three ways this might be done. The rendered visualizations are also viewable:
 
+- `Visualization of the breat cancer mapper using multiple datapoint color functions <../../_static/breast-cancer-multiple-color-functions.html>`_
+- `Visualization of the breat cancer mapper using multiple node color functions <../../_static/breast-cancer-multiple-node-color-functions.html>`_
+- `Visualization of the breat cancer mapper using multiple datapoint and node color functions <../../_static/breast-cancer-multiple-color-functions-and-multiple-node-color-functions.html>`_
 
 .. image:: ../../../examples/images/breast-cancer.png
 
@@ -40,7 +44,7 @@ import sklearn
 from sklearn import ensemble
 
 # For data we use the Wisconsin Breast Cancer Dataset
-# Via: 
+# Via:
 df = pd.read_csv("data/breast-cancer.csv")
 feature_names = [c for c in df.columns if c not in ["id", "diagnosis"]]
 df["diagnosis"] = df["diagnosis"].apply(lambda x: 1 if x == "M" else 0)
@@ -71,6 +75,32 @@ mapper.visualize(graph,
                  path_html="output/breast-cancer.html",
                  title="Wisconsin Breast Cancer Dataset",
                  custom_tooltips=y)
+
+
+# Visualization with multiple color functions
+mapper.visualize(graph,
+                 path_html="output/breast-cancer-multiple-color-functions.html",
+                 title="Wisconsin Breast Cancer Dataset",
+                 custom_tooltips=y,
+                 color_values=lens,
+                 color_function_name=['Isolation Forest', 'L2-norm'])
+
+
+# Visualization with multiple node color functions
+mapper.visualize(graph,
+                 path_html="output/breast-cancer-multiple-node-color-functions.html",
+                 title="Wisconsin Breast Cancer Dataset",
+                 custom_tooltips=y,
+                 node_color_function=['mean', 'std', 'median', 'max'])
+
+# Visualization showing both multiple color functions, and also multiple node color functions
+mapper.visualize(graph,
+                 path_html="output/breast-cancer-multiple-color-functions-and-multiple-node-color-functions.html",
+                 title="Wisconsin Breast Cancer Dataset",
+                 custom_tooltips=y,
+                 color_values=lens,
+                 color_function_name=['Isolation Forest', 'L2-norm'],
+                 node_color_function=['mean', 'std', 'median', 'max'])
 
 
 import matplotlib.pyplot as plt
